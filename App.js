21c1c06e232f7ad27238cab14bbf02f903ded158
Daniel Brož <danobroz66@gmail.com>
2022-09-22 14:02:35 +0200

    add button native element

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
     return (
@@ -7,6 +7,7 @@ export default function App() {
                 <Text>Another piece of text</Text>
             </View>
             <Text>Hello world!!!!!</Text>
+            <Button title='Tap me!' />
         </View>
     )
 }
